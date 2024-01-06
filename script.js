9c9644b887e8db8e8d0c68a0b3a50558615905ee
Ian Du <durpdur@gmail.com>
2024-01-05 16:55:43 -0800

    Complete CSS revamp, slide in animation added

--- script.js
+++ script.js
@@ -95,11 +95,12 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
         if (entry.isIntersecting) {
-            entry.target.classList.add();
+            entry.target.classList.add('fadeIn');
         } else {
-            entry.target.classList.remove();
+            entry.target.classList.remove('fadeIn');
         }
     });
 });
 
-const hiddenElements = document.querySelectorAll('hidden');
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
